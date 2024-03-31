674798bd3a6a476f60f3433275e95be29869671e
Kayrahan Toprak Tosun <75776777+KayrahanToprak@users.noreply.github.com>
2024-03-31 21:54:19 +0300

    Update TestUtil.java
    Added test methods to fully test the "compute" method

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,4 +12,20 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+  
+  @Test
+  public void lengthOne(){ assertFalse(c.compute(1214)); }
+  
+  @Test
+  public void lengthEven(){ assertFalse(c.compute(12, 33, 52, 122)); }
+  
+  @Test(expected = RuntimeException.class)
+  public void runtimeException(){ assertFalse(c.compute(12, 66, 0)); }
+  
+  @Test
+  public void sumModArgument(){ assertTrue(c.compute(1, 2, 3)); }
+  // Returns true because 1+2+3 = 6 and 6 % 2 == 0
+  
+  @Test
+  public void normalTest(){ assertFalse(c.compute(3, 7, 12)); }
 }
